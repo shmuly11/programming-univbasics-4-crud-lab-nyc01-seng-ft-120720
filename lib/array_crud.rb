774e6c0d6886f4429ef6110fae60a513ccc6fd33
shmuly levitin <shmuly11@gmail.com>
2020-11-16 19:19:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,18 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
+  array << element
   
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift = element
 end
 
 def remove_element_from_end_of_array(array)
